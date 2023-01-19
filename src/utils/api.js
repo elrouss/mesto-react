bf9f6982ce9017ea6e2a2b84ab29d53a58b3b0ef
Boris Zashliapin <test-zashliapin-b@yandex.ru>
2023-01-20 02:25:33 +0300

    refactor: оптимизировать код в файле api (вынести универсальный приватный метод)

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,6 +12,11 @@ class Api {
     this._headers = headers;
   };
 
+  _request(url, options) {
+    return fetch(url, options)
+      .then(this._checkResponse);
+  };
+
   _checkResponse(response) {
     if (response.ok) {
       return response.json();
@@ -21,67 +26,59 @@ class Api {
   };
 
   getUserInfo() {
-    return fetch(`${this._baseUrl}/users/me`, {
+    return this._request(`${this._baseUrl}/users/me`, {
       headers: this._headers
-    })
-      .then(this._checkResponse);
+    });
   };
 
   setUserInfo(name, about) {
-    return fetch(`${this._baseUrl}/users/me`, {
+    return this._request(`${this._baseUrl}/users/me`, {
       method: 'PATCH',
       headers: this._headers,
       body: JSON.stringify({ name, about })
-    })
-      .then(this._checkResponse);
+    });
   };
 
   setUserAvatar(avatar) {
-    return fetch(`${this._baseUrl}/users/me/avatar`, {
+    return this._request(`${this._baseUrl}/users/me/avatar`, {
       method: 'PATCH',
       headers: this._headers,
       body: JSON.stringify({ avatar })
-    })
-      .then(this._checkResponse);
+    });
   };
 
   getPhotocards() {
-    return fetch(`${this._baseUrl}/cards`, {
+    return this._request(`${this._baseUrl}/cards`, {
       headers: this._headers
-    })
-      .then(this._checkResponse);
+    });
   };
 
   addNewСard(name, link) {
-    return fetch(`${this._baseUrl}/cards`, {
+    return this._request(`${this._baseUrl}/cards`, {
       method: 'POST',
       headers: this._headers,
       body: JSON.stringify({ name, link })
-    })
-      .then(this._checkResponse);
+    });
   };
 
   deleteСard(id) {
-    return fetch(`${this._baseUrl}/cards/${id}`, {
+    return this._request(`${this._baseUrl}/cards/${id}`, {
       method: 'DELETE',
       headers: this._headers
-    })
-      .then(this._checkResponse);
+    });
   };
 
   changeLikeCardStatus(id, isLiked) {
     if (!isLiked) {
-      return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+      return this._request(`${this._baseUrl}/cards/${id}/likes`, {
         method: 'PUT',
         headers: this._headers
-      })
-        .then(this._checkResponse);
+      });
     } else {
-      return fetch(`${this._baseUrl}/cards/${id}/likes`, {
+      return this._request(`${this._baseUrl}/cards/${id}/likes`, {
         method: 'DELETE',
         headers: this._headers
-      })
-        .then(this._checkResponse);
+      });
     };
   };
 };
